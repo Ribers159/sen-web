a79d33c54e6a14222011180a31ddf9b554e63a76
RAUL ANGELO RIBEIRO <500067910@alunos.etec.itaqua>
2019-08-16 15:23:54 -0300

    Foi criado arquivo JS para validação de formulario de aluno e         importação de ladda button

---
+++ aluno.js
@@ -0,0 +1,16 @@
+window.onload = function (){
+    console.warn("OLAAA MUNDO")
+
+let meuForm = $("form#formAluno")
+
+console.log(meuForm)
+
+meuForm.on("submit", function(event){
+event.preventDefault()
+console.log("DEU CERTO")
+let botao = $("form button")
+console.log(botao)
+let botaoQGira = Ladda.create(botao[0])
+botaoQGira.start()
+})
+}
