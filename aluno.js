7a392f8d2163408aea35d8891fb0ca5de2adabd1
RAUL ANGELO RIBEIRO <500067910@alunos.etec.itaqua>
2019-09-06 15:21:27 -0300

    Foi realizado requisição na assincrona com servidor de API

--- aluno.js
+++ aluno.js
@@ -1,16 +1,21 @@
-window.onload = function (){
+window.onload = function () {
     console.warn("OLAAA MUNDO")
 
-let meuForm = $("form#formAluno")
+    let meuForm = $("form#formAluno")
 
-console.log(meuForm)
+    console.log(meuForm)
 
-meuForm.on("submit", function(event){
-event.preventDefault()
-console.log("DEU CERTO")
-let botao = $("form button")
-console.log(botao)
-let botaoQGira = Ladda.create(botao[0])
-botaoQGira.start()
-})
+    meuForm.on("submit", function (event) {
+        event.preventDefault()
+        console.log("DEU CERTO")
+        let botao = $("form button")
+        console.log(botao)
+        let botaoQGira = Ladda.create(botao[0])
+        botaoQGira.start()
+        fetch("http://localhost:8080/sen-api/teste.php").then(function (resposta) {
+            return resposta.json()
+        }).then(function(dados){
+            alert(dados.mensagem)
+        })
+    })
 }
